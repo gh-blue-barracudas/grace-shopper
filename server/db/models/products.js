a6c2b9ce3ca7677f337b7f3fe3e38536c31746e1
Dyane Ramos <dyane_r@yahoo.es>
2019-06-11 15:10:02 -0400

    product model

---
+++ server/db/models/products.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  productName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isDecimal: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://www.livescience.com/63459-dream-genes-rem-sleep.html"
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  productType: {
+    type: Sequelize.ENUM('dream', 'nightmare')
+  }
+})
+
+module.exports = Product
