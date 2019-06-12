ffb12b74dcc233fe163c21aa521cad1620cddc1d
Ahsun Kim <ahsunkim@gmail.com>
2019-06-12 10:51:49 -0400

    Added bare bones for cart routes

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,10 +1,11 @@
 import Axios from 'axios'
 
 const CREATE_CART = 'CREATE_CART'
-const RETRIEVE_CART = 'RETRIEVE_CART'
+// const RETRIEVE_CART = 'RETRIEVE_CART'
 const ADD_PROD = 'ADD_PROD'
 const DELETE_PROD = 'DELETE_PROD'
 const EDIT_PROD_QUANT = 'EDIT_PROD_QUANT'
+const DELETE_CART = 'DELETE_CART'
 
 // Initial State
 const defaultCart = {cart: null}
@@ -15,7 +16,7 @@ const defaultCart = {cart: null}
 export const createCart = () => {
   return async dispatch => {
     try {
-      // const {}
+      //
     } catch (error) {
       //
     }
--- server/api/cart.js
+++ server/api/cart.js
@@ -1,22 +1,61 @@
 const router = require('express').Router()
 const {User, Cart} = require('../db/models')
 
-router.get('/:userId', async (req, res, next) => {
+// router.get('/:userId', async (req, res, next) => {
+//   try {
+//     const user = await User.findByPk(req.params.userId, {
+//       include: {model: Cart}
+//     })
+//     if (user.cartId) {
+//       req.status(200).send(await Cart.findByPk(user.cartId))
+//     } else {
+//       // let newCart = Cart.create()
+//     }
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+// to add a cart
+router.post('/', async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.userId, {
-      include: {model: Cart}
-    })
-    if (user.cartId) {
-      req.status(200).send(await Cart.findByPk(user.cartId))
-    } else {
-      // let newCart = Cart.create()
-    }
+    let cart = await Cart.create()
+    res.status(200).send(cart)
   } catch (error) {
     next(error)
   }
 })
 
-router.post('/', async (req, res, next) => {
+// to add products to the cart
+router.put('/:cartId/addProduct', async (req, res, next) => {
+  try {
+    const [numberOfAffectedRows, affectedRows] = await Cart.update(
+      {id: req.params.cartId},
+      {where: {products: req.body.products}, returning: true, plain: true}
+    )
+    res.status(200).send(affectedRows[0])
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:cartId/deleteProduct', async (req, res, next) => {
+  try {
+    //
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:cartId/editProdQuantity', async (req, res, next) => {
+  try {
+    //
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:cartId', async (req, res, next) => {
   try {
     //
   } catch (error) {
