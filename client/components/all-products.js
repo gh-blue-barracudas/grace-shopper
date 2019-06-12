ef8eaaf9884928f7c5786935e8294505aeec6dff
Talia Fayaz <taliafayazc@gmail.com>
2019-06-12 15:37:27 -0400

    feat react renders all products category page

--- client/components/all-products.js
+++ client/components/all-products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {getProducts} from '../store/product'
+import {Link} from 'react-router-dom'
 
 class AllProducts extends React.Component {
   componentDidMount() {
@@ -9,13 +10,20 @@ class AllProducts extends React.Component {
   render() {
     const products = this.props.products
     return (
-      <div>
+      <div className="product_parent">
         {products.map(product => (
           <div key={product.id}>
-            <h1>{product.productName}</h1>
-            <h2>{product.description}</h2>
-            <h3>{product.price}</h3>
-            <img src={product.imageUrl} />
+            <div>
+              <Link to={`/products/${product.id}`}>
+                <img className="product_image" src={product.imageUrl} />
+              </Link>
+            </div>
+            <div className="product_info">
+              <Link to={`/products/${product.id}`}>
+                <h3>{product.productName}</h3>
+              </Link>
+              <h3>{product.price}</h3>
+            </div>
           </div>
         ))}
       </div>
