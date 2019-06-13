c5cb0f4dd99a07038688730fb8bea5deb408d11b
amaris chang <amaris.a.chang@gmail.com>
2019-06-13 13:20:14 -0400

    [feat] react - adds order confirmation page

--- client/routes.js
+++ client/routes.js
@@ -5,11 +5,11 @@ import PropTypes from 'prop-types'
 import {
   Login,
   Signup,
-  UserHome,
   Home,
   AllProducts,
   SingleProduct,
-  ConnectedCart
+  ConnectedCart,
+  OrderConfirm
 } from './components'
 import {me} from './store'
 
@@ -26,22 +26,24 @@ class Routes extends Component {
 
     return (
       <div className="routes_parent">
-      <Switch>
-        {/* Routes placed here are available to all visitors */}
-        <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={AllProducts} />
-        <Route path="/products/:id" component={SingleProduct} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route path="/products/:id/cart" component={ConnectedCart} />
-        <Route path="/cart" component={ConnectedCart} />
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in - should later add user profile page and order history here */} 
-          </Switch>
-        )}
-        <Route path="/login" component={Login} />
-      </Switch>
+        <Switch>
+          {/* Routes placed here are available to all visitors */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products" component={AllProducts} />
+          <Route path="/products/:id" component={SingleProduct} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route path="/products/:id/cart" component={ConnectedCart} />
+          <Route path="/cart" component={ConnectedCart} />
+          <Route path="/complete" component={OrderConfirm} />
+
+          {isLoggedIn && (
+            <Switch>
+              {/* Routes placed here are only available after logging in - should later add user profile page and order history here */}
+            </Switch>
+          )}
+          <Route path="/login" component={Login} />
+        </Switch>
       </div>
     )
   }
