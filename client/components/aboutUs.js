e9bf8ce0adcad0a707c19ee532b1f0d3772d32f2
Ahsun Kim <ahsunkim@gmail.com>
2019-06-13 16:01:18 -0400

    [feat] express, react - adds routes to edit and fetch user information and modifies about us page

--- client/components/aboutUs.js
+++ client/components/aboutUs.js
@@ -5,8 +5,26 @@ const AboutUs = () => {
     <div>
       <h1>ABOUT US</h1>
       <p>
-        This is about us. something about a dystonpian society etc etc not sure
-        if this is even spelled right blahblah
+        The world was stunned when the GH1904FS virus afflicted 30% of the
+        population in April of 2019. Our noble scientific community scrambled to
+        come up with a vaccine before mortality rates hit an all time high.
+        Although the vaccine was successful in effectively curbing the epidemic,
+        there was one significant side effect: Humans lost the ability to dream.
+      </p>
+      <p>
+        Our innovative scientists at Grace Shopper, nicknamed the Blue
+        Barracudas, united together to concoct "potions" that allow the everyday
+        consumer to dream once more.
+      </p>
+      <p>
+        Browse our selection of potions and be prepared to be amazed. Our dreams
+        make for an unforgettable night and are wonderful gifts for your loved
+        ones. We also offer a selection of nightmares that can be enjoyed by
+        daredevils and nemeses alike.
+      </p>
+      <p>
+        We're always constantly hard at work at developing a wider portfolio of
+        dreams and nightmares. After all, life is too short not to dream.
       </p>
       <button type="button">SHOP ALL</button>
     </div>
--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User} = require('../db/models')
+const {User, Order} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -13,5 +13,54 @@ router.get('/', async (req, res, next) => {
     res.json(users)
   } catch (err) {
     next(err)
+  }
+})
+
+// Route to grab information from userId
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId, {
+      include: {
+        model: Order
+      }
+    })
+    if (user) {
+      res.status(200).send({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        orders: user.orders
+      })
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// Route to update/store the billing address/shipping address for every user
+router.put('/:userId/address', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId)
+    if (user) {
+      user.update({
+        billingAddress1: req.body.billingAddress1,
+        billingAddress2: req.body.billingAddress2,
+        billingCity: req.body.billingCity,
+        billingState: req.body.billingState,
+        billingZip: req.body.billingZip,
+        shippingAddress1: req.body.shippingAddress1,
+        shippingAddress2: req.body.shippingAddress2,
+        shippingCity: req.body.shippingCity,
+        shippingState: req.body.shippingState,
+        shippingZip: req.body.shippingZip
+      })
+      res.status(202).send(user)
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
   }
 })
