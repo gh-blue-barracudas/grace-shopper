7abceb3a87f6f184b4ec217e78d8c7ee699bf544
Dyane Ramos <dyane_r@yahoo.es>
2019-06-16 01:43:05 -0400

    feat(cart) cart works with all its functionalities

--- client/components/emptycart.js
+++ client/components/emptycart.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
+//import Button from '@material-ui/core/Button'
+import {Link} from 'react-router-dom'
 
-const EmptyCart = () => {
-  return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <div>
-        <img src="https://png.pngtree.com/svg/20170913/bacb8f1c9c.svg" />
-        <p>Please add something to your cart first.</p>
+export class EmptyCart extends Component {
+  constructor() {
+    super()
+    this.handleBrowseClick = this.handleBrowseClick.bind(this)
+  }
+  handleBrowseClick() {
+    console.log('PROPS: ', this.props.history)
+    this.props.history.push('/products')
+  }
+  render() {
+    return (
+      <div className="cart">
+        <div className="cartName">
+          <h1>SHOPPING CART</h1>
+        </div>
+        <div>
+          <img
+            className="imgEmptyCart"
+            src="https://png.pngtree.com/svg/20170913/bacb8f1c9c.svg"
+          />
+          <p className="pEmptyCart">Please add something to your cart first.</p>
+          <div>
+            <Link to="/products">
+              <button type="button" className="buttonEmpty">
+                Browse Products
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-export default EmptyCart
